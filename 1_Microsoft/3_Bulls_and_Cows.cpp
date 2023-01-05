8511e3fd031a8a3fd9439bb7c092bc6fe4f53144
MasterCheapster <84666759+MasterCheapster@users.noreply.github.com>
2023-01-05 17:59:43 +0530

    3

--- 1_Microsoft/3_Bulls_and_Cows.cpp
+++ 1_Microsoft/3_Bulls_and_Cows.cpp
@@ -25,7 +25,11 @@ public:
             }
         }
         // Count the number of unpaired digits, and the less one is the cow number
-        // sec[c1-'0']++; gue[c2-'0']++; is used to count digits (0~9) number which are not paired. c += min(sec[i], gue[i]); for example, sec[1] =2, gue[1] =3, means there are 2 '1' in secret and 3 '1' in guess can not find paired one, then at least there are 2 '1' is in the wrong place.
+        // sec[c1-'0']++; gue[c2-'0']++; is used to count digits (0~9) number which are not paired.
+        // c += min(sec[i], gue[i]); 
+        //for example, sec[1] =2, gue[1] =3, 
+        //means there are 2 '1' in secret and 3 '1' in guess can not find paired one, 
+        //then at least there are 2 '1' is in the wrong place.
         for (int i = 0; i < sec.size(); i++)
         {
             c += min(sec[i], gue[i]);
